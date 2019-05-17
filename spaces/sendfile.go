98b836ac96efbc8e9e52f329466ea3f8f247a0b9
jefferson <jeff.otoni@gmail.com>
2019-05-16 21:02:06 -0300

    send file para spaces digitalocean utilizando aws sdk

--- spaces/sendfile.go
+++ spaces/sendfile.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+    "bufio"
     "encoding/json"
     "flag"
     "fmt"
-    "io/ioutil"
-    "strings"
-
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/credentials"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/s3"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "strings"
+)
+
+const (
+    ACL  = "public-read-write"
+    ACLp = "private"
+)
+
+var (
+    ACL_AP = ACL
+    BUCKET = ""
 )
 
 func main() {
@@ -36,15 +48,51 @@ func main() {
     // agora capturando dados..
 
     pathFile := flag.String("file", "", "nome do arquivo a ser enviado")
+    aclSend := flag.String("acl", "", "permissao: public or private")
+    fbucket := flag.String("bucket", "", "o nome do seu bucket")
     flag.Parse()
+
     if len(*pathFile) == 0 {
         flag.PrintDefaults()
         return
     }
 
-    b, err := ioutil.ReadFile(*pathFile) // just pass the file name
+    if len(*aclSend) > 0 && *aclSend != "public" {
+        ACL_AP = ACLp
+    }
+
+    if len(*fbucket) > 0 {
+        BUCKET = *fbucket
+    } else {
+        BUCKET = bucket
+    }
+
+    f, err := os.Open(*pathFile)
     if err != nil {
         fmt.Print(err)
+        return
+    }
+    defer f.Close()
+
+    // size file...
+    // fi, err := f.Stat()
+    // if err != nil {
+    //     fmt.Println(err)
+    //     return
+    // }
+
+    //// Use bufio.NewReader to get a Reader.
+    // ... Then use ioutil.ReadAll to read the entire content.
+    reader := bufio.NewReader(f)
+    b, err := ioutil.ReadAll(reader)
+    if err != nil {
+        fmt.Println("readAll:", err)
+        return
+    }
+
+    contentType, err := GetFileContentType(f)
+    if err != nil {
+        fmt.Println("contentType: ", err)
         return
     }
 
@@ -60,9 +108,11 @@ func main() {
 
     // Upload a file to the Space
     object := s3.PutObjectInput{
-        Body:   strings.NewReader(string(b)),
-        Bucket: aws.String(bucket),
-        Key:    aws.String(nameFileSpace),
+        ACL:         aws.String(ACL_AP),
+        Body:        strings.NewReader(string(b)),
+        Bucket:      aws.String(BUCKET),
+        Key:         aws.String(nameFileSpace),
+        ContentType: aws.String(contentType),
     }
     msgs3, err := s3Client.PutObject(&object)
     if err != nil {
@@ -102,4 +152,22 @@ func ReadKey() (endpoint, region, bucket, key, secret string, err error) {
     region = sk.Region
     bucket = sk.Bucket
     return
+}
+
+func GetFileContentType(out *os.File) (string, error) {
+
+    // garante que irá
+    // ler do inicio
+    out.Seek(0, 0)
+    // Only the first 512 bytes are used to sniff the content type.
+    buffer := make([]byte, 512)
+    _, err := out.Read(buffer)
+    if err != nil {
+        return "", err
+    }
+
+    // Use the net/http package's handy DectectContentType function. Always returns a valid
+    // content-type by returning "application/octet-stream" if no others seemed to match.
+    contentType := http.DetectContentType(buffer)
+    return contentType, nil
 }
