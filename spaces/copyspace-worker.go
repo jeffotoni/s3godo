eb8bcde97b96f024e62c9a5b6d428cfcb9d75f25
jefferson <jeff.otoni@gmail.com>
2019-05-23 13:53:18 -0300

    limit workder 500 ao inves de 1k

--- spaces/copyspace-worker.go
+++ spaces/copyspace-worker.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-    "bufio"
-    "encoding/json"
-    "flag"
-    "fmt"
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/credentials"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
-    . "github.com/jeffotoni/gcolor"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "os/signal"
-    "path/filepath"
-    "strconv"
-    "strings"
-    "sync"
-    "time"
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	. "github.com/jeffotoni/gcolor"
 )
 
 const (
-    ACL  = "public-read-write"
-    ACLp = "private"
+	ACL  = "public-read-write"
+	ACLp = "private"
 )
 
 var (
-    ACL_AP = ACL
-    BUCKET = ""
+	ACL_AP = ACL
+	BUCKET = ""
 )
 
 // -ldflags "-X main.k=your-key -X main.s=your-secret" main.go
@@ -44,342 +45,342 @@ var (
 // }
 
 type sendS3 struct {
-    Path     string
-    Pbucket  string
-    S3Client *s3.S3
-    I        int
+	Path     string
+	Pbucket  string
+	S3Client *s3.S3
+	I        int
 }
 
 func main() {
 
-    //println(k, s)
-    //return
+	//println(k, s)
+	//return
 
-    // abir file secrey
-    endpoint, region, bucket, key, secret, err := ReadKey()
+	// abir file secrey
+	endpoint, region, bucket, key, secret, err := ReadKey()
 
-    if err != nil {
-        fmt.Println("Erro ao montar suas credenciais de acesso ao DigitalOcean Space!")
-        return
-    }
+	if err != nil {
+		fmt.Println("Erro ao montar suas credenciais de acesso ao DigitalOcean Space!")
+		return
+	}
 
-    // Initialize a client using Spaces
-    s3Config := &aws.Config{
-        Credentials: credentials.NewStaticCredentials(key, secret, ""),
-        Endpoint:    aws.String(endpoint),
-        Region:      aws.String(region), // This is counter intuitive, but it will fail with a non-AWS region name.
-    }
+	// Initialize a client using Spaces
+	s3Config := &aws.Config{
+		Credentials: credentials.NewStaticCredentials(key, secret, ""),
+		Endpoint:    aws.String(endpoint),
+		Region:      aws.String(region), // This is counter intuitive, but it will fail with a non-AWS region name.
+	}
 
-    newSession := session.New(s3Config)
-    s3Client := s3.New(newSession)
+	newSession := session.New(s3Config)
+	s3Client := s3.New(newSession)
 
-    // agora capturando dados..
-    var pathFile string
+	// agora capturando dados..
+	var pathFile string
 
-    //pathFile := flag.String("file", "", "nome do arquivo ou diretorio a ser enviado")
-    flag.StringVar(&pathFile, "file", "", "nome do arquivo ou diretorio a ser enviado")
-    aclSend := flag.String("acl", "", "permissao: public or private")
-    fbucket := flag.String("bucket", "", "o nome do seu bucket")
-    flag.Parse()
+	//pathFile := flag.String("file", "", "nome do arquivo ou diretorio a ser enviado")
+	flag.StringVar(&pathFile, "file", "", "nome do arquivo ou diretorio a ser enviado")
+	aclSend := flag.String("acl", "", "permissao: public or private")
+	fbucket := flag.String("bucket", "", "o nome do seu bucket")
+	flag.Parse()
 
-    if len(pathFile) == 0 {
-        flag.PrintDefaults()
-        return
-    }
+	if len(pathFile) == 0 {
+		flag.PrintDefaults()
+		return
+	}
 
-    if len(*aclSend) > 0 && *aclSend != "public" {
-        ACL_AP = ACLp
-    }
+	if len(*aclSend) > 0 && *aclSend != "public" {
+		ACL_AP = ACLp
+	}
 
-    if len(*fbucket) > 0 {
-        BUCKET = *fbucket
-    } else {
-        BUCKET = bucket
-    }
+	if len(*fbucket) > 0 {
+		BUCKET = *fbucket
+	} else {
+		BUCKET = bucket
+	}
 
-    var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-    if DirExist(pathFile) {
+	if DirExist(pathFile) {
 
-        jobs := make(chan sendS3)
-        results := make(chan string)
-        //done := make(chan string, 1)
+		jobs := make(chan sendS3)
+		results := make(chan string)
+		//done := make(chan string, 1)
 
-        dir := pathFile
-        var i int
-        //wg.Add(5)
-        // inicia o worker
-        for w := 1; w <= 1000; w++ {
-            go worker(w, jobs, results)
-        }
+		dir := pathFile
+		var i int
+		//wg.Add(5)
+		// inicia o worker
+		for w := 1; w <= 500; w++ {
+			go worker(w, jobs, results)
+		}
 
-        go func(i int) {
-            err := filepath.Walk(dir,
-                func(path string, info os.FileInfo, err error) error {
-                    if err != nil {
-                        return err
-                    }
+		go func(i int) {
+			err := filepath.Walk(dir,
+				func(path string, info os.FileInfo, err error) error {
+					if err != nil {
+						return err
+					}
 
-                    i++
-                    pbucket := strings.Replace(path, os.Getenv("HOME"), "", -1)
-                    jobsy := sendS3{Path: path, Pbucket: pbucket, S3Client: s3Client, I: i}
-                    jobs <- jobsy
+					i++
+					pbucket := strings.Replace(path, os.Getenv("HOME"), "", -1)
+					jobsy := sendS3{Path: path, Pbucket: pbucket, S3Client: s3Client, I: i}
+					jobs <- jobsy
 
-                    //wg.Add(1)
-                    // go SendFileDo(path,
-                    //     pbucket,
-                    //     s3Client,
-                    //     &wg,
-                    // )
+					//wg.Add(1)
+					// go SendFileDo(path,
+					//     pbucket,
+					//     s3Client,
+					//     &wg,
+					// )
 
-                    return nil
-                })
-            if err != nil {
-                fmt.Println(err)
-            }
-        }(i)
+					return nil
+				})
+			if err != nil {
+				fmt.Println(err)
+			}
+		}(i)
 
-        wg.Wait()
+		wg.Wait()
 
-        defer close(jobs)
-        defer close(results)
+		defer close(jobs)
+		defer close(results)
 
-        for cx := range results {
-            println(cx)
-        }
+		for cx := range results {
+			println(cx)
+		}
 
-        //done <- "fim de envio"
-        println("fim de envio")
-        //wg.Wait()
-        // println(<-done)
-        return
+		//done <- "fim de envio"
+		println("fim de envio")
+		//wg.Wait()
+		// println(<-done)
+		return
 
-    } else {
+	} else {
 
-        // bucket
-        pbucket := strings.Replace(pathFile, os.Getenv("HOME"), "", -1)
-        p := pathFile
+		// bucket
+		pbucket := strings.Replace(pathFile, os.Getenv("HOME"), "", -1)
+		p := pathFile
 
-        //wg.Add(1)
-        SendFileDo(p, pbucket, s3Client, 1) // send one file
-        //wg.Wait()
-    }
+		//wg.Add(1)
+		SendFileDo(p, pbucket, s3Client, 1) // send one file
+		//wg.Wait()
+	}
 }
 
 func worker(id int, jobs <-chan sendS3, results chan<- string) {
-    for j := range jobs {
-        time.Sleep(time.Millisecond * 2)
-        results <- SendFileDo(j.Path, j.Pbucket, j.S3Client, j.I)
-    }
+	for j := range jobs {
+		time.Sleep(time.Millisecond * 2)
+		results <- SendFileDo(j.Path, j.Pbucket, j.S3Client, j.I)
+	}
 }
 
 func SendFileDoTest(pf, pbucket string, s3Client *s3.S3) string {
-    time.Sleep(time.Second)
-    return `send success: [` + pbucket + `]`
+	time.Sleep(time.Second)
+	return `send success: [` + pbucket + `]`
 }
 
 func SendFileDo(pf, pbucket string, s3Client *s3.S3, I int) string {
 
-    if DirExist(pf) {
-        return ""
-    }
+	if DirExist(pf) {
+		return ""
+	}
 
-    var msgReturn string
+	var msgReturn string
 
-    // defer wg.Done()
-    // time.Sleep(time.Second)
+	// defer wg.Done()
+	// time.Sleep(time.Second)
 
-    t1 := time.Now()
-    f, err := os.Open(pf)
-    if err != nil {
-        fmt.Print(err)
-        return ""
-    }
-    defer f.Close()
+	t1 := time.Now()
+	f, err := os.Open(pf)
+	if err != nil {
+		fmt.Print(err)
+		return ""
+	}
+	defer f.Close()
 
-    // size file...
-    fi, err := f.Stat()
-    if err != nil {
-        fmt.Println(err)
-        return ""
-    }
+	// size file...
+	fi, err := f.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
 
-    //// Use bufio.NewReader to get a Reader.
-    // ... Then use ioutil.ReadAll to read the entire content.
-    reader := bufio.NewReader(f)
-    b, err := ioutil.ReadAll(reader)
-    if err != nil {
-        fmt.Println("readAll:", err)
-        return ""
-    }
+	//// Use bufio.NewReader to get a Reader.
+	// ... Then use ioutil.ReadAll to read the entire content.
+	reader := bufio.NewReader(f)
+	b, err := ioutil.ReadAll(reader)
+	if err != nil {
+		fmt.Println("readAll:", err)
+		return ""
+	}
 
-    contentType, err := GetFileContentType(f)
-    if err != nil {
-        fmt.Println("contentType: ", err)
-        return ""
-    }
+	contentType, err := GetFileContentType(f)
+	if err != nil {
+		fmt.Println("contentType: ", err)
+		return ""
+	}
 
-    if len(string(b)) == 0 {
-        println("Error file está vazio..")
-        return ""
-    }
+	if len(string(b)) == 0 {
+		println("Error file está vazio..")
+		return ""
+	}
 
-    bs := string(b)
+	bs := string(b)
 
-    // runer
-    timer := RunerTimer()
+	// runer
+	// timer := RunerTimer()
 
-    type Fs struct {
-        Msgs3 *string
-        Name  string
-        Size  int64
-    }
+	type Fs struct {
+		Msgs3 *string
+		Name  string
+		Size  int64
+	}
 
-    //var msgs3 = make(chan *string)
-    var cfs = make(chan Fs, 1)
-    //var wg2 = &sync.WaitGroup{}
-    //wg2.Add(1)
-    <-timer
+	//var msgs3 = make(chan *string)
+	var cfs = make(chan Fs, 1)
+	//var wg2 = &sync.WaitGroup{}
+	//wg2.Add(1)
+	//<-timer
 
-    // aqui deveria ter um worker
-    go func(pf, b, contentType string) {
-        //println(pf)
-        //defer wg2.Done()
-        pathV := strings.Split(pf, "/")
-        lastp := len(pathV)
-        nameFileSpace := pathV[lastp-1]
+	// aqui deveria ter um worker
+	go func(pf, b, contentType string) {
+		//println(pf)
+		//defer wg2.Done()
+		pathV := strings.Split(pf, "/")
+		lastp := len(pathV)
+		nameFileSpace := pathV[lastp-1]
 
-        // Upload a file to the Space
-        object := s3.PutObjectInput{
-            ACL:         aws.String(ACL_AP),
-            Body:        strings.NewReader(b),
-            Bucket:      aws.String(BUCKET),
-            Key:         aws.String(pbucket),
-            ContentType: aws.String(contentType),
-        }
-        msgs3V, err := s3Client.PutObject(&object)
-        if err != nil {
-            fmt.Println(err.Error())
-            return
-        }
-        cfs <- Fs{Msgs3: msgs3V.ETag, Name: nameFileSpace, Size: fi.Size()}
-        //defer close(cfs)
-        time.Sleep(time.Millisecond * 1)
-    }(pf, bs, contentType)
-    //wg2.Wait()
+		// Upload a file to the Space
+		object := s3.PutObjectInput{
+			ACL:         aws.String(ACL_AP),
+			Body:        strings.NewReader(b),
+			Bucket:      aws.String(BUCKET),
+			Key:         aws.String(pbucket),
+			ContentType: aws.String(contentType),
+		}
+		msgs3V, err := s3Client.PutObject(&object)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		cfs <- Fs{Msgs3: msgs3V.ETag, Name: nameFileSpace, Size: fi.Size()}
+		//defer close(cfs)
+		time.Sleep(time.Millisecond * 1)
+	}(pf, bs, contentType)
+	//wg2.Wait()
 
-    //////////////////////////
-    csfS := <-cfs
-    kb := (csfS.Size / 1024)
-    t2 := time.Now()
+	//////////////////////////
+	csfS := <-cfs
+	kb := (csfS.Size / 1024)
+	t2 := time.Now()
 
-    msgReturn = "\r\033[?25h" + "[send success] count[" + strconv.Itoa(I) + "] Id[" + *csfS.Msgs3 + "] File[" + pbucket + "/" + csfS.Name + "] Size[" + strconv.FormatInt(kb, 10) + "Kb]" + "time[" + t2.Sub(t1).String() + "]"
-    msgReturn += "\r\033[?25h\033[?25h"
+	msgReturn = "\r\033[?25h" + "[send success] count[" + strconv.Itoa(I) + "] Id[" + *csfS.Msgs3 + "] File[" + pbucket + "/" + csfS.Name + "] Size[" + strconv.FormatInt(kb, 10) + "Kb]" + "time[" + t2.Sub(t1).String() + "]"
+	msgReturn += "\r\033[?25h\033[?25h"
 
-    //fmt.Print("\r")
-    //fmt.Print("\033[?25h")
-    //fmt.Println("[send success] Id["+*csfS.Msgs3+"] File["+csfS.Name+"] Size[", kb, "Kb]", "time[", t2.Sub(t1), "]")
-    //fmt.Print("\r")
-    //fmt.Print("\033[?25h")
-    //fmt.Print("\033[?25h")
-    /////////////////////////////
+	//fmt.Print("\r")
+	//fmt.Print("\033[?25h")
+	//fmt.Println("[send success] Id["+*csfS.Msgs3+"] File["+csfS.Name+"] Size[", kb, "Kb]", "time[", t2.Sub(t1), "]")
+	//fmt.Print("\r")
+	//fmt.Print("\033[?25h")
+	//fmt.Print("\033[?25h")
+	/////////////////////////////
 
-    // done..
-    return msgReturn
+	// done..
+	return msgReturn
 }
 
 func DirExist(path string) bool {
 
-    //if _, err := os.Stat(path); err == nil {
-    if stat, err := os.Stat(path); err == nil && stat.IsDir() {
-        return true
-    }
+	//if _, err := os.Stat(path); err == nil {
+	if stat, err := os.Stat(path); err == nil && stat.IsDir() {
+		return true
+	}
 
-    return false
+	return false
 }
 
 func ReadKey() (endpoint, region, bucket, key, secret string, err error) {
-    pathHome := os.Getenv("HOME")
-    pathHome = pathHome + "/.dokeys"
-    b, err := ioutil.ReadFile(pathHome) // just pass the file name
-    if err != nil {
-        fmt.Print("keys: ", err)
-        return
-    }
+	pathHome := os.Getenv("HOME")
+	pathHome = pathHome + "/.dokeys"
+	b, err := ioutil.ReadFile(pathHome) // just pass the file name
+	if err != nil {
+		fmt.Print("keys: ", err)
+		return
+	}
 
-    //jsonkey := string(b) // convert content to a 'string'
-    type skey struct {
-        Key      string `json:"key"`
-        Secret   string `json:"secret"`
-        Endpoint string `json:"endpoint"`
-        Region   string `json:"region"`
-        Bucket   string `json:"bucket"`
-    }
+	//jsonkey := string(b) // convert content to a 'string'
+	type skey struct {
+		Key      string `json:"key"`
+		Secret   string `json:"secret"`
+		Endpoint string `json:"endpoint"`
+		Region   string `json:"region"`
+		Bucket   string `json:"bucket"`
+	}
 
-    sk := &skey{}
-    if err = json.Unmarshal(b, sk); err != nil {
-        return
-    }
+	sk := &skey{}
+	if err = json.Unmarshal(b, sk); err != nil {
+		return
+	}
 
-    key = sk.Key
-    secret = sk.Secret
-    endpoint = sk.Endpoint
-    region = sk.Region
-    bucket = sk.Bucket
-    return
+	key = sk.Key
+	secret = sk.Secret
+	endpoint = sk.Endpoint
+	region = sk.Region
+	bucket = sk.Bucket
+	return
 }
 
 func GetFileContentType(out *os.File) (string, error) {
 
-    // garante que irá
-    // ler do inicio
-    out.Seek(0, 0)
-    // Only the first 512 bytes are used to sniff the content type.
-    buffer := make([]byte, 512)
-    _, err := out.Read(buffer)
-    if err != nil {
-        return "", err
-    }
+	// garante que irá
+	// ler do inicio
+	out.Seek(0, 0)
+	// Only the first 512 bytes are used to sniff the content type.
+	buffer := make([]byte, 512)
+	_, err := out.Read(buffer)
+	if err != nil {
+		return "", err
+	}
 
-    // Use the net/http package's handy DectectContentType function. Always returns a valid
-    // content-type by returning "application/octet-stream" if no others seemed to match.
-    contentType := http.DetectContentType(buffer)
-    return contentType, nil
+	// Use the net/http package's handy DectectContentType function. Always returns a valid
+	// content-type by returning "application/octet-stream" if no others seemed to match.
+	contentType := http.DetectContentType(buffer)
+	return contentType, nil
 }
 
 func RunerTimer() <-chan time.Time {
 
-    timer := time.Tick(time.Duration(50) * time.Millisecond)
+	timer := time.Tick(time.Duration(50) * time.Millisecond)
 
-    go func() {
-        sc := make(chan os.Signal, 1)
-        signal.Notify(sc, os.Interrupt)
+	go func() {
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, os.Interrupt)
 
-        <-sc
+		<-sc
 
-        fmt.Print(RedCor("\ncanceled!"))
-        fmt.Print("\033[?25h")
-        os.Exit(0)
-    }()
+		fmt.Print(RedCor("\ncanceled!"))
+		fmt.Print("\033[?25h")
+		os.Exit(0)
+	}()
 
-    fmt.Print("\033[?25l")
+	fmt.Print("\033[?25l")
 
-    s := []rune(`|/~\`)
-    //s := []rune(`-=*=`)
-    //s := []rune(`◐◓◑◒`)
-    i := 0
+	s := []rune(`|/~\`)
+	//s := []rune(`-=*=`)
+	//s := []rune(`◐◓◑◒`)
+	i := 0
 
-    go func() {
-        for {
-            <-timer
-            fmt.Print("\r")
-            fmt.Print(YellowCor(string(s[i])))
-            i++
-            if i == len(s) {
-                i = 0
-            }
-        }
-    }()
+	go func() {
+		for {
+			<-timer
+			fmt.Print("\r")
+			fmt.Print(YellowCor(string(s[i])))
+			i++
+			if i == len(s) {
+				i = 0
+			}
+		}
+	}()
 
-    return timer
+	return timer
 }
