a67440348290fac69d652fce8e940bba8b51f7d9
jefferson <jeff.otoni@gmail.com>
2019-05-25 14:16:39 -0300

    load install update

---
+++ spaces/v1/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##### @jeffotoni
+sudo rm -rf /opt/dospace/v1
+sudo mkdir -p /opt/dospace/v1/
+sudo wget https://github.com/jeffotoni/s3godo/tree/master/spaces/v1/copyspace -P /opt/dospace/v1/
+chmod 755 -R /opt/dospace
+sudo ln -s /opt/dospace/v1/copysapce /usr/bin/copyspace
+
+echo "\033[0;33m######### Thanks for Download ##########\033[0m"
+echo "comand: copyspace -h"
+copyspace
