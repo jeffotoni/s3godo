36f8d85170f29b07918501421a6631225d26d449
jefferson <jeff.otoni@gmail.com>
2019-05-25 14:20:11 -0300

    install copyspace linux

--- spaces/v1/install.sh
+++ spaces/v1/install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 ##### @jeffotoni
 sudo rm -rf /opt/dospace/v1
-sudo mkdir -p /opt/dospace/v1/
+sudo mkdir -p /opt/dospace/v1
 sudo wget https://github.com/jeffotoni/s3godo/tree/master/spaces/v1/copyspace -P /opt/dospace/v1/
-chmod 755 -R /opt/dospace
-sudo ln -s /opt/dospace/v1/copysapce /usr/bin/copyspace
-
+sudo chmod 755 -R /opt/dospace
+sudo ln -s /opt/dospace/v1/copyspace /usr/bin/copyspace
 echo "\033[0;33m######### Thanks for Download ##########\033[0m"
 echo "comand: copyspace -h"
 copyspace
